f7790d28d8129f7d97162c2f631e679d0eea6000
Eric Dalquist <eric.dalquist@gmail.com>
2009-03-11 16:22:01 +0000

    PERSONDIR-49 Only do defensive copies when data is provided, just add unmodifiable wrapper during retrieval (per interface contract) Add methods for removing an attribute and setting all attributes atomicly.
    git-svn-id: https://source.jasig.org/person-directory/trunk@17410 f5dbab47-78f9-eb45-b975-e544023573eb

--- person-directory-impl/src/main/java/org/jasig/services/persondir/support/AdditionalDescriptors.java
+++ person-directory-impl/src/main/java/org/jasig/services/persondir/support/AdditionalDescriptors.java
@@ -5,12 +5,17 @@
  */
 package org.jasig.services.persondir.support;
 
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.commons.lang.Validate;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 import org.jasig.services.persondir.IPersonAttributes;
 
 /**
@@ -30,7 +35,7 @@ public class AdditionalDescriptors implements IPersonAttributes {
     
     // Private Members.
     private String name = null;
-    private final Map<String,List<Object>> attributes = new ConcurrentHashMap<String,List<Object>>();
+    private Map<String,List<Object>> attributes = new ConcurrentHashMap<String,List<Object>>();
 
     /*
      * Public API.
@@ -38,36 +43,27 @@ public class AdditionalDescriptors implements IPersonAttributes {
 
     public Object getAttributeValue(String name) {
         List<Object> values = attributes.get(name);
-        return values == null || values.size() == 0 ? null 
-                                        : values.get(0);
+        return values == null || values.size() == 0 ? null : values.get(0);
     }
 
     public List<Object> getAttributeValues(String name) {
-        return new LinkedList<Object>(attributes.get(name));
-    }
-    
-    public void setAttributeValues(String name, List<Object> values) {
-        
-        // Assertions.
-        if (name == null) {
-            String msg = "Argument 'name' cannot be null.";
-            throw new IllegalArgumentException(msg);
+        final List<Object> values = this.attributes.get(name);
+        if (values == null) {
+            return null;
         }
-        
-        attributes.put(name, values);
-        
+
+        return Collections.unmodifiableList(values);
     }
 
     public Map<String, List<Object>> getAttributes() {
-        return new HashMap<String, List<Object>>(attributes);
+        return Collections.unmodifiableMap(this.attributes);
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
     
     public void setName(String name) {
-
         // Assertions.
         if (name == null) {
             // NB:  This assertion may not be necessary/appropriate...
@@ -76,7 +72,82 @@ public class AdditionalDescriptors implements IPersonAttributes {
         }
 
         this.name = name;
+    }
+    
+    public void setAttributes(Map<String, List<Object>> attributes) {
+        Validate.notNull(attributes, "Argument 'attributes' cannot be null");
+        final Map<String, List<Object>> newAttributes = new ConcurrentHashMap<String, List<Object>>();
+        
+        for (final Map.Entry<String, List<Object>> newAttribute : newAttributes.entrySet()) {
+            final String name = newAttribute.getKey();
+            final List<Object> values = newAttribute.getValue();
+            
+            if (values == null) {
+                newAttributes.put(name, null);
+            }
+            else {
+                newAttributes.put(name, new ArrayList<Object>(values));
+            }
+        }
+        
+        this.attributes = newAttributes;
+    }
+    
+    public List<Object> setAttributeValues(String name, List<Object> values) {
+        // Assertions.
+        if (name == null) {
+            String msg = "Argument 'name' cannot be null.";
+            throw new IllegalArgumentException(msg);
+        }
 
+        if (values == null) {
+            return this.attributes.put(name, null);
+        }
+
+        return this.attributes.put(name, new ArrayList<Object>(values));
+    }
+    
+    public List<Object> removeAttribute(String name) {
+        Validate.notNull(name, "Argument 'name' cannot be null");
+        return this.attributes.remove(name);
     }
 
+    
+    /**
+     * @see java.lang.Object#equals(Object)
+     */
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+        if (!(object instanceof IPersonAttributes)) {
+            return false;
+        }
+        IPersonAttributes rhs = (IPersonAttributes) object;
+        return new EqualsBuilder()
+            .append(this.name, rhs.getName())
+            .isEquals();
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(1574945487, 827742191)
+            .append(this.name)
+            .toHashCode();
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+            .append("name", this.name)
+            .append("attributes", this.attributes)
+            .toString();
+    }
 }
