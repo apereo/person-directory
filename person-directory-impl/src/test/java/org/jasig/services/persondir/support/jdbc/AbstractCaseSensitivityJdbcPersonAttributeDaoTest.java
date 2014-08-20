132ed0153d719f5008a43eb025885456187d0f4f
James Wennmacher <jwennmacher@unicon.net>
2014-08-20 13:10:06 -0700

    NOJIRA release prep

--- person-directory-impl/src/test/java/org/jasig/services/persondir/support/jdbc/AbstractCaseSensitivityJdbcPersonAttributeDaoTest.java
+++ person-directory-impl/src/test/java/org/jasig/services/persondir/support/jdbc/AbstractCaseSensitivityJdbcPersonAttributeDaoTest.java
@@ -1,11 +1,30 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.jasig.services.persondir.support.jdbc;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+
 import javax.sql.DataSource;
 
 import org.hsqldb.jdbcDriver;
@@ -13,11 +32,7 @@ import org.jasig.services.persondir.IPersonAttributes;
 import org.jasig.services.persondir.support.AbstractDefaultQueryPersonAttributeDaoTest;
 import org.jasig.services.persondir.util.CaseCanonicalizationMode;
 import org.jasig.services.persondir.util.Util;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.ConnectionCallback;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-
-import junit.framework.TestCase;
 
 /**
  * Otherwise would be huge amounts of duplicated boilerplate for verifying
