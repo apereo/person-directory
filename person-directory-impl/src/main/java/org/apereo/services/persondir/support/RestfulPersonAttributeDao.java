359b714b1a13e39e0bc594f1e851dd8795e8b084
Misagh Moayyed <mm1844@gmail.com>
2018-02-08 13:14:14 +0330

    fix rest attribute repo to account for people with multivalued attributes

--- person-directory-impl/src/main/java/org/apereo/services/persondir/support/RestfulPersonAttributeDao.java
+++ person-directory-impl/src/main/java/org/apereo/services/persondir/support/RestfulPersonAttributeDao.java
@@ -18,9 +18,11 @@ import org.apereo.services.persondir.IPersonAttributes;
 import org.springframework.http.HttpMethod;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -29,11 +31,13 @@ import java.util.Set;
  * Provides the ability to contact a URL resource to ask for attributes.
  * Support GET/POST endpoints, and provides the username in form of a parameter.
  * The response is expected to be a JSON map.
+ *
  * @author Misagh Moayyed
  */
 public class RestfulPersonAttributeDao extends BasePersonAttributeDao {
     private final ObjectMapper jacksonObjectMapper = new ObjectMapper().findAndRegisterModules();
-
+   private final IUsernameAttributeProvider usernameAttributeProvider = new SimpleUsernameAttributeProvider();
+   
     private boolean caseInsensitiveUsername = false;
     private String url;
     private String basicAuthUsername;
@@ -118,7 +122,13 @@ public class RestfulPersonAttributeDao extends BasePersonAttributeDao {
 
     @Override
     public Set<IPersonAttributes> getPeopleWithMultivaluedAttributes(final Map<String, List<Object>> query) {
-        throw new UnsupportedOperationException("This method is not implemented.");
+        final Set<IPersonAttributes> people = new LinkedHashSet<>();
+        final String username = usernameAttributeProvider.getUsernameFromQuery(query);
+        final IPersonAttributes person = getPerson(username);
+        if (person != null) {
+            people.add(person);
+        }
+        return people;
     }
 
     @Override
@@ -133,15 +143,15 @@ public class RestfulPersonAttributeDao extends BasePersonAttributeDao {
         return Collections.EMPTY_SET;
     }
 
-    private static Map<String, List<Object>> stuffAttributesIntoListValues(final Map<String, Object> personAttributesMap) {
+    private static Map<String, List<Object>> stuffAttributesIntoListValues(final Map<String, ?> personAttributesMap) {
         final Map<String, List<Object>> personAttributes = new HashMap<>();
 
-        for (final Map.Entry<String, Object> stringObjectEntry : personAttributesMap.entrySet()) {
+        for (final Map.Entry<String, ?> stringObjectEntry : personAttributesMap.entrySet()) {
             final Object value = stringObjectEntry.getValue();
             if (value instanceof List) {
                 personAttributes.put(stringObjectEntry.getKey(), (List) value);
             } else {
-                personAttributes.put(stringObjectEntry.getKey(), Arrays.asList(value));
+                personAttributes.put(stringObjectEntry.getKey(), new ArrayList<>(Arrays.asList(value)));
             }
         }
         return personAttributes;
