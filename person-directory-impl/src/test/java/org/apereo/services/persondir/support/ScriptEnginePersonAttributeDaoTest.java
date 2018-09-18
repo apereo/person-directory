4fbe6599e44f18fbc08d16db8bb226c5ccd3e51a
Hal Deadman <deadman_harold@bah.com>
2018-09-18 19:56:43 -0400

    Add engineName property to ScriptEnginePersonAttributeDao
    This supports the ability to pass contents of script to Dao rather
    than the filename.
    Update commons-io version to match CAS (for use in unit test)

--- person-directory-impl/src/test/java/org/apereo/services/persondir/support/ScriptEnginePersonAttributeDaoTest.java
+++ person-directory-impl/src/test/java/org/apereo/services/persondir/support/ScriptEnginePersonAttributeDaoTest.java
@@ -1,16 +1,31 @@
 package org.apereo.services.persondir.support;
 
+import org.apache.commons.io.IOUtils;
 import org.apereo.services.persondir.AbstractPersonAttributeDaoTest;
 import org.apereo.services.persondir.IPersonAttributeDao;
 import org.apereo.services.persondir.IPersonAttributes;
 import org.apereo.services.persondir.util.Util;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/annotationContext.xml"})
 public class ScriptEnginePersonAttributeDaoTest extends AbstractPersonAttributeDaoTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
     private ScriptEnginePersonAttributeDao dao;
 
     public ScriptEnginePersonAttributeDaoTest() {
@@ -22,6 +37,7 @@ public class ScriptEnginePersonAttributeDaoTest extends AbstractPersonAttributeD
         return this.dao;
     }
 
+    @Test
     public void testGetAttributesWithJavascript() {
         this.dao.setScriptFile("SampleScriptedJavascriptPersonAttributeDao.js");
         final IPersonAttributes person = this.dao.getPerson("testuser");
@@ -30,6 +46,7 @@ public class ScriptEnginePersonAttributeDaoTest extends AbstractPersonAttributeD
         assertEquals(person.getAttributes().size(), 4);
     }
 
+    @Test
     public void testGetAttributesWithGroovy() {
         this.dao.setScriptFile("SampleScriptedGroovyPersonAttributeDao.groovy");
         final IPersonAttributes person = this.dao.getPerson("testuser");
@@ -38,6 +55,17 @@ public class ScriptEnginePersonAttributeDaoTest extends AbstractPersonAttributeD
         assertEquals(person.getAttributes().size(), 4);
     }
 
+    @Test
+    public void testGetAttributesWithGroovyString() throws IOException {
+        this.dao.setScriptFile(IOUtils.toString(applicationContext.getResource("file:./src/test/resources/SampleScriptedGroovyPersonAttributeDao.groovy").getInputStream(), StandardCharsets.UTF_8));
+        this.dao.setEngineName("groovy");
+        final IPersonAttributes person = this.dao.getPerson("testuser");
+        assertNotNull(person);
+        assertEquals(person.getName(), "testuser");
+        assertEquals(person.getAttributes().size(), 4);
+    }
+
+    @Test
     public void testGetMultiValuedAttributesWithGroovy() {
         this.dao.setScriptFile("SampleScriptedGroovyPersonAttributeDao.groovy");
         final Map<String, List<Object>> query = new LinkedHashMap<>();
