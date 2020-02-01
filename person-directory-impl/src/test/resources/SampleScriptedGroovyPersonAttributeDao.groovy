04d18057f816b35695ea8861d6e860b2f4ad474f
hdeadman <hal.deadman@gmail.com>
2020-02-01 14:46:16 -0500

    update groovy to 2.5.9
    also fix order of some test assertions

--- person-directory-impl/src/test/resources/SampleScriptedGroovyPersonAttributeDao.groovy
+++ person-directory-impl/src/test/resources/SampleScriptedGroovyPersonAttributeDao.groovy
@@ -4,7 +4,7 @@ Map<String, List<Object>> run(final Object... args) {
     def uid = args[0]
     def logger = args[1]
 
-    logger.debug("Things are happening just fine")
+    logger.debug("Things are happening just fine with uid: " + uid)
     return[username:[uid], likes:["cheese", "food"], id:[1234,2,3,4,5], another:"attribute"]
 }
 
